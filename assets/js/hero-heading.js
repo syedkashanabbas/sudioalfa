50cc7ec5cc44acfdf78e1ec21dc27de8dffd9aac
SyedKashanAbbasNaqvi <abbaskashan234@gmail.com>
2025-09-20 21:54:24 +0500

    converting it to in italian site

--- assets/js/hero-heading.js
+++ assets/js/hero-heading.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const heading = document.getElementById("hero-heading");
   const sub = document.getElementById("hero-sub");
+  const subSub = document.getElementById("hero-sub-sub");
 
   // Heading cinematic zoom
   setTimeout(() => {
     heading.classList.add("animate-cine-zoom");
   }, 300);
+  // Heading cinematic zoom
 
   // Subheading cinematic rise
   setTimeout(() => {
     sub.classList.add("animate-cine-rise");
   }, 1200);
+  //sub-sub heading cinematic rise
+    setTimeout(() => {
+    subSub.classList.add("animate-cine-rise");
+  }, 1600);
 });
